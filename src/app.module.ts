91ebca3eef75a8815a4b8bcbe7d665f8bce48e78
Hiroto-Iizuka <oroxith@gmail.com>
2023-03-01 23:36:21 +0900

    feat: auth, uesr, prisma のservice, module, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UsersService } from './users/users.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService, UsersService],
 })

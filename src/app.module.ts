04eee632e522e0faf070dfda054f68d583e39259
Hiroto-Iizuka <oroxith@gmail.com>
2023-03-03 23:17:30 +0900

    feat: prismaの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
-import { UsersService } from './users/users.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService, UsersService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
